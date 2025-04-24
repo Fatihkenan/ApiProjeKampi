0cf901acf34e0a359d933ad17852458dd2d20b30
fatih kenan KAYA <fkaya253412@gmail.com>
2025-04-24 17:17:56 +0300

    automapper giriş

---
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,24 @@
+﻿using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjeKampi.WebApi.Dtos.MessageDtos;
+using ApiProjeKampi.WebApi.Entities;
+using AutoMapper;
+using AutoMapper.Features;
+
+namespace ApiProjeKampi.WebApi.Mapping
+{
+    public class GeneralMapping : Profile //auto mapperdan gelen bir sınıf
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,ResultFeatureDto>().ReverseMap(); //Feature ile ResultFeatureDto maplensin ve tam terside maplensin
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMesaageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+        }
+    }
+}
